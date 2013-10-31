710b40082a53a35b4015812486d984646ba9e48e
cookster404 <cookster404@142.3.85.25>
2013-10-31 09:50:13 -0600

    on my new branch

--- src/print.java
+++ src/print.java
@@ -10,7 +10,7 @@
 public class print {
      public static void main(String[] args) {
         
-         System.out.println("i made this file!");
+         System.out.println("i made this to compare");
          
     }
     
