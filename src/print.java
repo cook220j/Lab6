65a8e75f2ff60c504da32598f3bd401e0116d6ac
cookster404 <cookster404@142.3.85.25>
2013-10-31 09:35:49 -0600

    My new file

---
+++ src/print.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cookster404
+ */
+public class print {
+     public static void main(String[] args) {
+        
+         System.out.println("i made this file!");
+         
+    }
+    
+}
